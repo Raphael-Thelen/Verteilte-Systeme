7b404db2954c1bca5f41aa069e4c252ed47b0a45
Raphael Thelen <raphael.thelen@idot-digital.de>
2025-07-15 18:49:30 +0200

    Präsentation

--- Übungsblatt 2/sim4da-S25/src/org/oxoo2a/sim4da/DSMNode.kt
+++ Übungsblatt 2/sim4da-S25/src/org/oxoo2a/sim4da/DSMNode.kt
@@ -4,5 +4,5 @@ interface DSMNode {
     fun write(key: String, value: String)
     fun read(key: String): String?
     fun nodeName(): String // monitoring for testing reasons
-    fun getLocalState(key: String): String?
+    fun getLocalState(key: String): String? // monitoring for testing reasons
 }
